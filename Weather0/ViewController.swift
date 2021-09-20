729e1c50cc25d9b708a52b82cb60b038e6de72b2
AlexS <isanphone@gmail.com>
2021-09-20 17:51:04 +0300

    Add connections from storyboard to code, add iq keyboard from swift package, add delegate methods for city searchTextField

--- Weather0/ViewController.swift
+++ Weather0/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var conditionImageView: UIImageView!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
-
-
+    @IBAction func locationButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        //done with editing and dismiss keyboard
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    //return key in the keyboard was pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        //done with editing and dismiss keyboard
+        searchTextField.endEditing(true)
+        return false // true<<
+    }
+    //reset searchText
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    //placeholder for type something
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Enter the City Name"
+            return false
+        }
+    }
+    
 }
 
